d1d3dfa141634fc9747abdb832df1881657a7646
Luke Mwila <outlier.developer@gmail.com>
2017-09-16 20:57:37 +0200

    Initial push

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Customer } from './Customer';
 
 @Component({
     selector: 'sandbox',
@@ -6,19 +7,30 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent{
-    name:string = 'John Doe';
-    age:number = 35;
-    hasChildren:boolean = true;
-    city:any = 'Lusaka';
-    myNumbersArray:number[] = [11,12,13];
-    nyStringArray:string[] = ['Hey,','how','are','you','?'];
-    myAnyArray:any[] = [11,28,'Jazz Jack Rabbit'];
-    myTuple: [string, number] = ['James Harden', 32];
-    unusable: void = undefined;
-    u: undefined = undefined;
-    n: null = null;
+
+    customer:Customer;
+    customers:Customer[];
 
     constructor(){
-        
+        this.customer = {
+            id: 1,
+            name: 'Carlisle Malone',
+            email: 'carl.mal@mail.com'
+        }
+
+        this.customers = [
+            {
+                id: 2,
+                name: 'Jessie Malone',
+                email: 'jessie.mal@mail.com'
+            },
+            {
+                id: 3,
+                name: 'Francis Lubinda',
+                email: 'franco.lubinda@mail.com'
+            }
+        ];
     }
+
 }
+

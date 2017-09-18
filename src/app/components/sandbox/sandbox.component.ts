42661e77268093548eea48e1d2ed939a8e332c5d
Luke Mwila <outlier.developer@gmail.com>
2017-09-18 22:20:03 +0200

    Initial push

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,21 +2,50 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    templateUrl: `./sandbox.component.html`,
-    styleUrls: ['./sandbox.component.css']
-    /*styles: [`
-        .special{
-            color: green;
-            font-size: 20px;
-            text-transform: uppercase
-        }
-    `]
-    */
+    template: `
+        <h1>Hello World</h1>
+        {{ people }}
+        <ul>
+            <li *ngFor="let person of people">
+                {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people; let i = index">
+                {{ i + 1 }} {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people2">
+                {{person.firstName}} {{person.lastName}}
+            </li>
+        </ul>
+    `
 })
 
 export class SandboxComponent{
+    people = ['Rick', 'Darrell', 'Carl', 'Glen'];
+    people2 = [
+        {
+            firstName: 'Rick',
+            lastName: 'Mwila'
+        },
+        {
+            firstName: 'Jerry',
+            lastName: 'Johnson'
+        },
+        {
+            firstName: 'Xoliswe',
+            lastName: 'Zulu'
+        },
+        {
+            firstName: 'Chanda',
+            lastName: 'Mwila'
+        }
+    ];
 
-    
-
+    constructor(){
+        this.people[2] = 'Xavier';
+    }
 }
 

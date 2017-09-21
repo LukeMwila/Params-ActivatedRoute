db444967ed5f099b37a7fbe0c17d39548fda1b8d
Luke Mwila <outlier.developer@gmail.com>
2017-09-21 21:17:16 +0200

    Initial push

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -1,32 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core' 
 
 @Component({
     selector: 'sandbox',
     template: `
-    <h1>Hello 
-       <!--
-       <span *ngIf="showName">{{name}}</span>
-       <span *ngIf="!showName">World</span> 
-       -->
-       <span *ngIf="showName; else noName">{{ name }}</span>
-       <ng-template #noName>World</ng-template>
-    </h1>
-    <p>Hello {{ showName ? name : 'World' }}</p>
-
-    <hr>
-
-    <div [ngSwitch]="greeting">
-        <div *ngSwitchCase="'1'">Hello World</div>
-        <div *ngSwitchCase="'2'">Hi You</div>
-        <div *ngSwitchCase="'3'">Hey Me</div>
-        <div *ngSwitchDefault>Sup</div>
-    </div>
+        <h1>Hello World</h1>
+        <!-- 3 ways to bind properties -->
+        <div><img src="{{ imageUrl }}"></div>
+        <div><img [src]="imageUrl"></div>
+        <div><img bind-src="imageUrl"></div>
     `
 })
 
 export class SandboxComponent{
-    name:string = 'John Doe';
-    showName:boolean = false;
-    greeting:number = 2;
+    imageUrl = 'http://lorempixel.com/400/200'
 }
 

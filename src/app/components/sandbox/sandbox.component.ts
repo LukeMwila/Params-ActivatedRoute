2469d4e20196d1f4931d39e30c499a17e126d625
Luke Mwila <outlier.developer@gmail.com>
2017-10-07 00:13:55 +0200

    Initial push

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,20 +3,31 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'sandbox',
     template: `
-        <h1>Hello World</h1>
-        <button id="btn" (click)="changeValue()">Change Value</button>
-        <div *ngIf="value">
-            <h1>{{ text }}</h1>
+        <h1>Hello {{ text }}</h1>
+        <div>
+            <input type="text" (keyup)="fireEvent($event)" placeholder="keyup event">
+        </div>
+        <div>
+            <input type="text" (focus)="fireEvent($event)" placeholder="focus">
+        </div>
+        <div>
+            <input type="text" (blur)="fireEvent($event)" placeholder="blur">
+        </div>
+        <div>
+            <input type="text" (copy)="fireEvent($event)" placeholder="copy">
+        </div>
+        <div>
+            <input type="text" (paste)="fireEvent($event)" placeholder="paste">
         </div>
     `
 })
 
 export class SandboxComponent{
-    text:string = 'Hello World';
-    value:boolean = true;
+    text:string = '';
 
-    changeValue(){
-        this.value = !this.value;
+    fireEvent(e){
+        console.log(e.type);
+        this.text = e.target.value;
     }
 }
 

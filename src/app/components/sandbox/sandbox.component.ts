5d5aad4ae38dbddcf5480ad5ca9bcc6dfe8cb406
Luke Mwila <outlier.developer@gmail.com>
2017-10-07 18:14:00 +0200

    Initial push

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -6,16 +6,19 @@ import { DataService } from '../../services/data.service';
     template: `
         <h1>Hello World</h1>
         <ul class="list-group">
-            <li class="list-group-item" *ngFor="let user of users">{{ user }}</li>
+            <li class="list-group-item" *ngFor="let d of data">{{ d }}</li>
         </ul>
     `
 })
 
 export class SandboxComponent{
-    users:string[];
+    data:any[] = [];
 
     constructor(public dataService:DataService){
-        this.users = this.dataService.getUsers();
+        this.dataService.getData().subscribe(data => {
+            //console.log(data);
+            this.data.push(data);
+        });
     }
 }
 
